4b51abd86d586c2416deefb32596d8549758d243
Ryan Ariana <out1688@fifgroup.co.id>
2024-01-16 22:00:49 +0700

    tambah cors pada indexjs2

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ app.use(
     extended: true
   })
 );
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
 app.get("/", (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
   res.json({ message: "ok" });
 });
 
