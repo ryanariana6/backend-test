0dc01f461297e957f23f2c5f182244044174817f
Ryan Ariana <out1688@fifgroup.co.id>
2024-01-16 22:17:54 +0700

    cors

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors"); 
 const newsRouter = require("./routes/NewsRouter");
 app.use(express.json());
+
+const corsOptions = {
+  origin: 'https://frontend-react-flame.vercel.app', // Ganti dengan origin frontend Anda
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 app.use(
   express.urlencoded({
     extended: true
   })
 );
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-})
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
